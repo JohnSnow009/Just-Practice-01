264a51e38b64e3dd3bdcadcfa3d5fdd3c031da27
JohnSnow009 <112542414+JohnSnow009@users.noreply.github.com>
2022-09-06 21:19:12 -1000

    Increment Divide Fix

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 let homeScoreEl = document.getElementById("homescore-el")
 let guestScoreEl = document.getElementById("guestscore-el")
 
-let score = 0
+let scoreHome = 0
+let scoreGuest = 0
 
 function plusHomeOne() {
-    score += 1
-    homeScoreEl.textContent = score
+    scoreHome += 1
+    homeScoreEl.textContent = scoreHome
 }
 
 function plusHomeTwo() {
-    score += 2
-    homeScoreEl.textContent = score
+    scoreHome += 2
+    homeScoreEl.textContent = scoreHome
 }
 
 function plusHomeThree() {
-    score += 3
-    homeScoreEl.textContent = score
+    scoreHome += 3
+    homeScoreEl.textContent = scoreHome
 }
 
 function plusGuestOne() {
-    score += 1
-    guestScoreEl.textContent = score
+    scoreGuest += 1
+    guestScoreEl.textContent = scoreGuest
 }
 
 function plusGuestTwo() {
-    score += 2
-    guestScoreEl.textContent = score
+    scoreGuest += 2
+    guestScoreEl.textContent = scoreGuest
 }
 
 function plusGuestThree() {
-    score += 3
-    guestScoreEl.textContent = score
+    scoreGuest += 3
+    guestScoreEl.textContent = scoreGuest
 }
